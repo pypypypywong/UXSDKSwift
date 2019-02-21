c3343ce69321a257b3e0ee2ae4a1e6310437211b
Bill Malkin <billmalkin@bill-malkins-macbook-pro.local>
2019-02-22 09:25:01 +1030

    Initial Commit

---
+++ UXSDKSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UXSDKSwift
+//
+//  Created by Bill Malkin on 22/2/19.
+//  Copyright © 2019 Bill Malkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
