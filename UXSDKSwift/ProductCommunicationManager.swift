bddfc2953401e07a50bf13c4eec144918aafbe4e
Bill Malkin <billmalkin@Bill-Malkins-MacBook-Pro.local>
2019-02-23 10:01:40 +1030

    first commit

---
+++ UXSDKSwift/ProductCommunicationManager.swift
@@ -0,0 +1,9 @@
+//
+//  ProductCommunicationManager.swift
+//  UXSDKSwift
+//
+//  Created by Bill Malkin on 22/2/19.
+//  Copyright © 2019 Bill Malkin. All rights reserved.
+//
+
+import Foundation
